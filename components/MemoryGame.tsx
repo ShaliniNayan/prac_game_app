22ec3009b68fad390996a513fb97058fee158f62
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-09-01 00:41:30 +0530

    Generate deck for memrory card

--- components/MemoryGame.tsx
+++ components/MemoryGame.tsx
@@ -1,1 +1,20 @@
 'use client';
+
+import React from 'react';
+
+const generateDeck = () => {
+  const memoryCards = [
+    'ciaz',
+    'fortuner',
+    'innova',
+    'hector',
+    'scropio',
+    'telsa',
+    'verna',
+    'wagonr',
+  ];
+};
+
+export default function MemoryGame() {
+  return <div></div>;
+}
