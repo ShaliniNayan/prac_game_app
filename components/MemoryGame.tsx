517706921a6123a55ff28fac9c83794849d3b8e5
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-09-04 00:44:25 +0530

    Add function for image displayed

--- components/MemoryGame.tsx
+++ components/MemoryGame.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import React, { useState } from 'react';
+import Image from 'next/image';
 
 const generateDeck = () => {
   const memoryCards = [
@@ -22,8 +23,12 @@ export default function MemoryGame() {
   const [cards, setCards] = useState<string[]>(generateDeck());
   return (
     <div>
-      <div className='grid grid-cols-4'>
-        {cards.map[(card: any) => <div></div>]}
+      <div className='grid grid-cols-4 gap-5'>
+        {cards.map((card, index) => (
+          <div key={index} className='w-28 h-28 transform cursor-pointer '>
+            <Image src={`/images/${card}.webp`} fill alt={card} />
+          </div>
+        ))}
       </div>
     </div>
   );
