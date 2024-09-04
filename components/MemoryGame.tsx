67853df5d1dfcbb2d51478a335297ff70d7becb1
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-09-04 21:14:29 +0530

    Add function for flipped image

--- components/MemoryGame.tsx
+++ components/MemoryGame.tsx
@@ -23,12 +23,24 @@ export default function MemoryGame() {
   const [cards, setCards] = useState<string[]>(generateDeck());
   const [flipped, setFlipped] = useState<number[]>([]);
 
+  const handleClick = (index: number) => {
+    setFlipped([...flipped, index]);
+  };
+
   return (
     <div>
       <div className='grid grid-cols-4 gap-5'>
         {cards.map((card, index) => (
-          <div key={index} className='w-28 h-28 transform cursor-pointer '>
-            <Image src={`/images/${card}.webp`} fill alt={card} />
+          <div
+            key={index}
+            onClick={() => handleClick(index)}
+            className='w-28 h-28 transform cursor-pointer'
+          >
+            {flipped.includes(index) ? (
+              <Image src={`/images/${card}.webp`} fill alt='Memory Card' />
+            ) : (
+              '?'
+            )}
           </div>
         ))}
       </div>
